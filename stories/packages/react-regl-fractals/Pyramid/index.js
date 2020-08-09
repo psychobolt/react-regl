5d397856e8a227a4b366602b8c00516cb92119c5
psychobolt <560721+psychobolt@users.noreply.github.com>
2020-08-08 23:02:53 -0700

    Load readme for react-regl-fractal examples

--- stories/packages/react-regl-fractals/Pyramid/index.js
+++ stories/packages/react-regl-fractals/Pyramid/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { ReglContainer, Frame } from '@psychobolt/react-regl';
 import { Pyramid } from '@psychobolt/react-regl-fractals';
+import Readme from 'packages/react-regl-fractals/src/Pyramid/README.md';
 
 import { withKnobs } from '../shared';
 
@@ -9,6 +10,8 @@ const clear = ({ regl }) => regl.clear({
   color: [1, 1, 1, 1],
   depth: 1,
 });
+
+export const README = Readme;
 
 export default () => (
   <ReglContainer onMount={clear}>
--- stories/packages/react-regl-fractals/index.js
+++ stories/packages/react-regl-fractals/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withKnobs } from '@storybook/addon-knobs';
-import { doc } from 'storybook-readme';
+import { doc, withReadme } from 'storybook-readme';
+import path from 'path';
 
 import Readme from 'packages/react-regl-fractals/README.md';
 
-import Pyramid from './Pyramid';
+const reqExample = require.context('./', true, /^\.\/[a-zA-Z0-9]*\/$/);
 
-storiesOf('packages/react-regl-fractals', module)
-  .add('Readme', doc(Readme));
+const category = storiesOf('packages/react-regl-fractals', module)
+  .add('Readme', doc(Readme))
+  .addDecorator(withKnobs);
 
-storiesOf('packages/react-regl-fractals/Pyramid', module)
-  .addDecorator(withKnobs)
-  .add('2D', () => <Pyramid />);
+reqExample.keys().forEach(folder => {
+  const example = path.resolve('./', folder);
+  const name = /\/(.+)/.exec(example)[1];
+  if (name === 'shared') return;
+  const { default: Component, README } = require(`.${example}`); // eslint-disable-line global-require, import/no-dynamic-require
+  const render = () => <Component />;
+  category.add(name, README ? withReadme(README, render) : render);
+});
